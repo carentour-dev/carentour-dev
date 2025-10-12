93d0e79bbd0065e9fa942dcba710f5b616c40580
Muhammad Yassin <dev@carentour.com>
2025-10-12 20:43:19 +0300

    Update next.config.js

--- carentour/next.config.js
+++ carentour/next.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'lovable.dev'],
+    domains: ["images.unsplash.com", "www.carentour.com"],
   },
   // Exclude old React Router pages directory from compilation
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'].map(ext => `app.${ext}`).concat(['tsx', 'ts', 'jsx', 'js']),
+  pageExtensions: ["tsx", "ts", "jsx", "js"]
+    .map((ext) => `app.${ext}`)
+    .concat(["tsx", "ts", "jsx", "js"]),
   webpack: (config, { isServer }) => {
     // Ignore the old pages directory
     config.module.rules.push({
       test: /src\/pages\/.*/,
-      loader: 'ignore-loader',
+      loader: "ignore-loader",
     });
     return config;
   },
-}
+};
 
 export default nextConfig;
