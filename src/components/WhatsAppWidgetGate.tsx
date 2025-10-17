b76cc795c4bc4b8718e8c1edaedef3730f4e6279
Muhammad Yassin <dev@carentour.com>
2025-10-17 23:34:32 +0300

    Create WhatsAppWidgetGate.tsx

---
+++ src/components/WhatsAppWidgetGate.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import WhatsAppWidget, {
+  type WhatsAppWidgetProps,
+} from "@/components/WhatsAppWidget";
+
+const HIDDEN_PREFIXES = ["/admin", "/cms"];
+
+export default function WhatsAppWidgetGate(props: WhatsAppWidgetProps) {
+  const pathname = usePathname();
+
+  if (!pathname) {
+    return null;
+  }
+
+  if (HIDDEN_PREFIXES.some((prefix) => pathname.startsWith(prefix))) {
+    return null;
+  }
+
+  return <WhatsAppWidget {...props} />;
+}
