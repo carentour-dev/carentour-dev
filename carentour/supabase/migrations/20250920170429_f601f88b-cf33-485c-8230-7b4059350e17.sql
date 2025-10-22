99eb5e6dccc2ce0f5412a1e45b360f9910133f20
Muhammad Yassin <dev@carentour.com>
2025-10-22 07:36:24 +0300

    Delete 20250920170429_f601f88b-cf33-485c-8230-7b4059350e17.sql

--- carentour/supabase/migrations/20250920170429_f601f88b-cf33-485c-8230-7b4059350e17.sql
+++
@@ -1,6 +0,0 @@
--- Delete all profiles first (to avoid foreign key issues)
-DELETE FROM public.profiles;
-
--- Delete all users from auth.users table using Supabase admin function
--- Note: This will cascade delete related auth data automatically
-SELECT auth.users.id FROM auth.users;
