9b83ca080c8a6a3350b3c73f81a905f360647bb1
Muhammad Yassin <dev@carentour.com>
2025-11-04 17:40:28 +0200

    Create types.ts

---
+++ src/lib/operations/types.ts
@@ -0,0 +1,9 @@
+export type OperationsSectionId =
+  | "overview"
+  | "requests"
+  | "start-journey"
+  | "consultations"
+  | "appointments"
+  | "patients";
+
+export type OperationsSectionSlug = OperationsSectionId;
