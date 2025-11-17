ae63d9d7643188f3064c4ca59502496df21ff456
Muhammad Yassin <dev@carentour.com>
2025-11-17 08:07:51 +0200

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -29,7 +29,7 @@ export default async function About() {
     return (
       <div className="min-h-screen">
         <Header />
-        <main className="py-10">
+        <main>
           <BlockRenderer blocks={cmsPage.content} />
         </main>
         <Footer />
