f9cbb52a6539985125540ca0c90989d87fb737a0
Muhammad Yassin <dev@carentour.com>
2026-01-18 02:27:05 +0200

    Update types.ts

--- src/lib/operations/types.ts
+++ src/lib/operations/types.ts
@@ -7,6 +7,7 @@ export type OperationsSectionId =
   | "patients"
   | "testimonials"
   | "tasks"
-  | "cnt-ai";
+  | "cnt-ai"
+  | "quotation-calculator";
 
 export type OperationsSectionSlug = OperationsSectionId;
