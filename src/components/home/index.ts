8fd0aa877d467e5817a5796ac4973de60e4e3a03
Muhammad Yassin <dev@carentour.com>
2026-03-26 21:47:50 +0200

    Create index.ts

---
+++ src/components/home/index.ts
@@ -0,0 +1,18 @@
+export {
+  DEFAULT_DIFFERENTIATORS,
+  DEFAULT_HOME_CTA_CONTENT,
+  DEFAULT_HOME_HERO_CONTENT,
+  DEFAULT_JOURNEY_STEPS,
+  type Differentiator,
+  type HomeCtaContent,
+  type HomeHeroContent,
+  type JourneyStep,
+} from "./content";
+export { DifferentiatorsSection } from "./DifferentiatorsSection";
+export {
+  buildFeaturedTreatmentCards,
+  FeaturedTreatmentsSection,
+} from "./FeaturedTreatmentsSection";
+export { HomeCtaSection } from "./HomeCtaSection";
+export { HomeHeroSection } from "./HomeHeroSection";
+export { JourneyStepsSection } from "./JourneyStepsSection";
