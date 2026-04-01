074636b6afd9672fe3fbe73a8f58b0c014dfd2e7
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:33:54 +0200

    Delete page.tsx

--- src/app/finance/reports/profit-loss/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { FinanceReportScreen } from "@/components/finance/FinanceReportScreen";
-
-export default function FinanceProfitLossReportPage() {
-  return <FinanceReportScreen report="profit-loss" />;
-}
