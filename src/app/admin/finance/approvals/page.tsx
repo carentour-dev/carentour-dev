cd53200094f13968255be5c3b6c691b2377f3121
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:30:11 +0200

    Delete page.tsx

--- src/app/admin/finance/approvals/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { FinanceApprovalsConsole } from "@/components/finance/FinanceApprovalsConsole";
-
-export default function AdminFinanceApprovalsPage() {
-  return <FinanceApprovalsConsole workspaceBasePath="/admin/finance" />;
-}
