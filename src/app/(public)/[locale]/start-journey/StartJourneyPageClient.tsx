1a7df52dfd6cdb03f9b9cc6d4edbd378b7a454a3
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:27:52 +0200

    Create StartJourneyPageClient.tsx

---
+++ src/app/(public)/[locale]/start-journey/StartJourneyPageClient.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { StartJourneyWizard } from "@/components/start-journey/StartJourneyWizard";
+
+export default function StartJourneyPageClient() {
+  return <StartJourneyWizard mode="standalone" />;
+}
