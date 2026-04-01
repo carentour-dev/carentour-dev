1192ab8ee16fccd90390e51fa8bf416d489ac23d
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:25:28 +0200

    Create page.tsx

---
+++ src/app/(internal)/operations/quotation-calculator/[id]/print/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { QuotationPrintView } from "@/components/operations/quotation-calculator/QuotationPrintView";
+
+export default function OperationsQuotationPrintView() {
+  return <QuotationPrintView />;
+}
