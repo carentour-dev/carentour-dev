75760d6fd865bb009c4f444deffbab26d60b5b24
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:33:47 +0200

    Delete page.tsx

--- src/app/finance/reports/ap-aging/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { FinanceReportScreen } from "@/components/finance/FinanceReportScreen";
-
-export default function FinanceApAgingReportPage() {
-  return <FinanceReportScreen report="ap-aging" />;
-}
