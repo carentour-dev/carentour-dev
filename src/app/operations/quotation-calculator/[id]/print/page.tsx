531dee5ff996470aabf122cb498a7961a2c71e73
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:34:31 +0200

    Delete page.tsx

--- src/app/operations/quotation-calculator/[id]/print/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { QuotationPrintView } from "@/components/operations/quotation-calculator/QuotationPrintView";
-
-export default function OperationsQuotationPrintView() {
-  return <QuotationPrintView />;
-}
