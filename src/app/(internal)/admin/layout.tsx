3317309600e19d5ea13cf9054ff5b3bc4f94733c
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:22:18 +0200

    Create layout.tsx

---
+++ src/app/(internal)/admin/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+import AdminLayoutClient from "./AdminLayoutClient";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
+};
+
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return <AdminLayoutClient>{children}</AdminLayoutClient>;
+}
