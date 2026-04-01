425aee6adb348549c7c303678f5acb36c73f9abf
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:34:04 +0200

    Delete page.tsx

--- src/app/finance/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { FinanceSettingsConsole } from "@/components/finance/FinanceSettingsConsole";
-
-export default function FinanceSettingsPage() {
-  return <FinanceSettingsConsole workspaceBasePath="/finance" />;
-}
