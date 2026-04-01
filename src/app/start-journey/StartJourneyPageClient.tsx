5c4904ee9b5aab214a73740e1bb9dbcb322cf6d0
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:35:09 +0200

    Delete StartJourneyPageClient.tsx

--- src/app/start-journey/StartJourneyPageClient.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { StartJourneyWizard } from "@/components/start-journey/StartJourneyWizard";
-
-export default function StartJourneyPageClient() {
-  return <StartJourneyWizard mode="standalone" />;
-}
