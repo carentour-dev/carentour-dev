628ec08ab7e0b559126719011b7c3efdd3d300a4
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:33:25 +0200

    Delete FinanceLayoutClient.tsx

--- src/app/finance/FinanceLayoutClient.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import type { ReactNode } from "react";
-import { FinanceShell } from "@/components/finance/FinanceShell";
-
-export default function FinanceLayout({ children }: { children: ReactNode }) {
-  return <FinanceShell>{children}</FinanceShell>;
-}
