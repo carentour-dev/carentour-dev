459bc99d9467498236b2fb502dd17efe22d74ab1
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:33:34 +0200

    Delete page.tsx

--- src/app/finance/ledger/journals/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { FinanceJournalWorkspace } from "@/components/finance/FinanceJournalWorkspace";
-
-export default function FinanceJournalPage() {
-  return <FinanceJournalWorkspace workspaceBasePath="/finance" />;
-}
