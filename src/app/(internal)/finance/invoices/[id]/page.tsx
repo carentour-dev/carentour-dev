e86f2843bac188cc6e33af53733dd4d081d3a158
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:24:06 +0200

    Create page.tsx

---
+++ src/app/(internal)/finance/invoices/[id]/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { FinanceInvoiceDetail } from "@/components/finance/FinanceInvoiceDetail";
+
+export default function FinanceInvoiceDetailPage() {
+  return <FinanceInvoiceDetail workspacePath="/finance" />;
+}
