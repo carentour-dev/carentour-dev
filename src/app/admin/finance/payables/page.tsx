dd8b62562e8252f259fe313cfc566cdd8ed7114c
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:30:26 +0200

    Delete page.tsx

--- src/app/admin/finance/payables/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { FinancePayablesWorkspace } from "@/components/finance/FinancePayablesWorkspace";
-
-export default function AdminFinancePayablesPage() {
-  return <FinancePayablesWorkspace workspaceBasePath="/admin/finance" />;
-}
