27be7d1f79a85c7040393b0ca99749d9e8bd29ce
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:33:57 +0200

    Delete page.tsx

--- src/app/finance/reports/trial-balance/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { FinanceReportScreen } from "@/components/finance/FinanceReportScreen";
-
-export default function FinanceTrialBalanceReportPage() {
-  return <FinanceReportScreen report="trial-balance" />;
-}
