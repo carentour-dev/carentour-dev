8819c89cecc2bcaccb0f9621992ee7ca72405d82
Muhammad Yassin <dev@carentour.com>
2026-04-01 10:21:36 +0200

    Create page.tsx

---
+++ src/app/(internal)/admin/finance/ledger/journals/[id]/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { FinanceJournalDetail } from "@/components/finance/FinanceJournalDetail";
+
+export default function AdminFinanceJournalDetailPage() {
+  return <FinanceJournalDetail workspaceBasePath="/admin/finance" />;
+}
