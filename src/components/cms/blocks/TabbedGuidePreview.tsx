b36ce477d912df0cf3c0757d87dbf66187ef6676
Muhammad Yassin <dev@carentour.com>
2026-04-04 03:17:28 +0200

    Update TabbedGuidePreview.tsx

--- src/components/cms/blocks/TabbedGuidePreview.tsx
+++ src/components/cms/blocks/TabbedGuidePreview.tsx
@@ -1,10 +1,15 @@
+import type { PublicLocale } from "@/i18n/routing";
 import type { BlockInstance } from "@/lib/cms/blocks";
 import { TabbedGuideContent } from "./TabbedGuideContent";
 
 export function TabbedGuidePreview({
   block,
+  locale = "en",
 }: {
   block: BlockInstance<"tabbedGuide">;
+  locale?: PublicLocale;
 }) {
-  return <TabbedGuideContent block={block} hotelMap={{}} isPreview />;
+  return (
+    <TabbedGuideContent block={block} hotelMap={{}} isPreview locale={locale} />
+  );
 }
