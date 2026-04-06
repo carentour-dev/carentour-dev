f56c054650948503b9d2b754856e570a2d2f33d0
Muhammad Yassin <dev@carentour.com>
2026-04-06 16:24:55 +0200

    Update label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -14,6 +14,7 @@ const Label = React.forwardRef<
     VariantProps<typeof labelVariants>
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
+    data-slot="label"
     ref={ref}
     className={cn(labelVariants(), className)}
     {...props}
