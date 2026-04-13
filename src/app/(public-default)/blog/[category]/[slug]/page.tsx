368f317711ecd6717eeaf10a034458495d8a106b
Muhammad Yassin <dev@carentour.com>
2026-04-13 03:55:38 +0200

    Update page.tsx

--- src/app/(public-default)/blog/[category]/[slug]/page.tsx
+++ src/app/(public-default)/blog/[category]/[slug]/page.tsx
@@ -1,9 +1,8 @@
 import * as RouteModule from "@/app/(public)/[locale]/blog/[category]/[slug]/page";
 import { buildDefaultLocaleRoute } from "@/app/default-locale-public";
-import { BLOG_SURFACE_REVALIDATE_SECONDS } from "@/lib/blog/revalidation";
 
 const route = buildDefaultLocaleRoute(RouteModule);
 
-export const revalidate = BLOG_SURFACE_REVALIDATE_SECONDS;
+export const revalidate = 60;
 export const generateMetadata = route.generateMetadata;
 export default route.Page;
