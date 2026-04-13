361f31dcfe2899ac2ecabc78241dca15c97bbabe
Muhammad Yassin <dev@carentour.com>
2026-04-13 03:15:51 +0200

    Update route.tsx

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -2,8 +2,6 @@ import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
 import { normalizePath } from "@/lib/seo";
 
-export const runtime = "edge";
-
 const IMAGE_SIZE = {
   width: 1200,
   height: 630,
