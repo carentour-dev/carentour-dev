96f50d71842424e7b1b81f9e927f66c2deb5e59e
Muhammad Yassin <dev@carentour.com>
2026-04-13 03:12:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -24,18 +24,6 @@ const nextConfig = {
       },
     ],
   },
-  // Exclude old React Router pages directory from compilation
-  pageExtensions: ["tsx", "ts", "jsx", "js"]
-    .map((ext) => `app.${ext}`)
-    .concat(["tsx", "ts", "jsx", "js"]),
-  webpack: (config, { isServer }) => {
-    // Ignore the old pages directory
-    config.module.rules.push({
-      test: /src\/pages\/.*/,
-      loader: "ignore-loader",
-    });
-    return config;
-  },
 };
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
