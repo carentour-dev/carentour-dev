1b255d1499328ee7b07f5c6cf3b144a606895f59
Muhammad Yassin <dev@carentour.com>
2026-04-15 01:23:53 +0200

    Create OptimizedImage.tsx

---
+++ src/components/OptimizedImage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import NextImage, { type ImageProps } from "next/image";
+import { getOptimizedImageProps } from "@/lib/supabase/imageOptimization";
+
+export default function OptimizedImage({ src, ...props }: ImageProps) {
+  const optimizationProps =
+    typeof src === "string" ? getOptimizedImageProps(src) : {};
+
+  return <NextImage src={src} {...props} {...optimizationProps} />;
+}
