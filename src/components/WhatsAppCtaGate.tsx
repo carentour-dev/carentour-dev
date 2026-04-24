01ca8d0e5196b5cccbc418f8d9db2ea4bdbde33a
Muhammad Yassin <dev@carentour.com>
2026-04-24 04:12:12 +0300

    Update WhatsAppCtaGate.tsx

--- src/components/WhatsAppCtaGate.tsx
+++ src/components/WhatsAppCtaGate.tsx
@@ -1,20 +1,5 @@
-"use client";
-
-import { usePathname } from "next/navigation";
 import WhatsAppCta, { type WhatsAppCtaProps } from "./WhatsAppCta";
 
-const HIDDEN_PREFIXES = ["/admin", "/operations", "/finance", "/cms"];
-
 export default function WhatsAppCtaGate(props: WhatsAppCtaProps) {
-  const pathname = usePathname();
-
-  if (!pathname) {
-    return null;
-  }
-
-  if (HIDDEN_PREFIXES.some((prefix) => pathname.startsWith(prefix))) {
-    return null;
-  }
-
   return <WhatsAppCta {...props} />;
 }
