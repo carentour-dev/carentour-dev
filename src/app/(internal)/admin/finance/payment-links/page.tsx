6b986e4c68c527cf225c6e6ad129a0dd0ff3014e
Muhammad Yassin <dev@carentour.com>
2026-05-24 14:48:09 +0300

    Create page.tsx

---
+++ src/app/(internal)/admin/finance/payment-links/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { FinancePaymentLinksWorkspace } from "@/components/finance/FinancePaymentLinksWorkspace";
+
+export default function AdminFinancePaymentLinksPage() {
+  return <FinancePaymentLinksWorkspace />;
+}
