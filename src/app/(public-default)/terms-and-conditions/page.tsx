530a531b7a708b37ebefc7784fe0c71c8832c7b2
Muhammad Yassin <dev@carentour.com>
2026-06-23 22:20:53 +0300

    Create page.tsx

---
+++ src/app/(public-default)/terms-and-conditions/page.tsx
@@ -0,0 +1,8 @@
+import * as RouteModule from "@/app/(public)/[locale]/terms-and-conditions/page";
+import { buildDefaultLocaleRoute } from "@/app/default-locale-public";
+
+const route = buildDefaultLocaleRoute(RouteModule);
+
+export const revalidate = 300;
+export const generateMetadata = route.generateMetadata;
+export default route.Page;
